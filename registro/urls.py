04258f238a1448b2797281f35c5bc8a2f7bf1cb9
Volcanogamer <wilsonbinrongluo@gmail.com>
2025-06-17 21:38:40 -0300

    pagina inicial e criar_usuario

--- registro/urls.py
+++ registro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from registro import views
 from .views import (
-    base,
+    index,
     criar_usuario,
     criar_coleta_faces,
     face_detection,
@@ -22,7 +22,7 @@ router.register(r'registros', RegistroPontoViewSet)
 
 urlpatterns = [
     # Rotas Web
-    path('', base, name='base'),
+    path('', index, name='index'),
     path('criar_usuario/', criar_usuario, name='criar_usuario'),
     path('criar_coleta_faces/<int:usuario_id>', criar_coleta_faces, name='criar_coleta_faces'),
     path('face_detection/', face_detection, name='face_detection'),
--- registro/views.py
+++ registro/views.py
@@ -193,5 +193,5 @@ def video_reconhecimento(request):
         content_type="multipart/x-mixed-replace; boundary=frame"
     )
     
-def base(request):
-    return render(request, 'base.html')
+def index(request):
+    return render(request, 'index.html')
